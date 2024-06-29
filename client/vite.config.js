4792ace01b190f2ca871caea4bc79e3bcd2886f7
Hitesh Patel <hitesh.patel@ksb.com>
2024-06-29 14:10:35 +0530

    fixed build issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,25 +4,19 @@ import path from "path";
 import { fileURLToPath } from "url";
 import jsconfigPaths from "vite-jsconfig-paths";
 
-// Convert the import.meta.url to a file path
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-console.log("dirname", __dirname);
-
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "@/": path.resolve(__dirname, "src"),
-//       "@/pages/": path.resolve(__dirname, "src/pages"),
-//       "@/components/": path.resolve(__dirname, "src/components"),
-//       "@/assets/": path.resolve(__dirname, "src/assets"),
-//       "@/store/": path.resolve(__dirname, "src/store"),
-//       "@/utils/": path.resolve(__dirname, "src/utils"),
-//       "@/routes/": path.resolve(__dirname, "src/routes"),
-//     },
-//   },
-// });
-
 export default defineConfig({
   plugins: [react(), jsconfigPaths()],
+
+  build: {
+    rollupOptions: {
+      external: ["@radix-ui/react-progress", "@radix-ui/react-popover", "cmdk"],
+      output: {
+        globals: {
+          "@radix-ui/react-progress": "RadixProgress",
+          "@radix-ui/react-popover": "RadixPopover",
+          cmdk: "cmdk",
+        },
+      },
+    },
+  },
 });
